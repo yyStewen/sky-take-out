46c9796e271b4c7066f295c9aca8c6c620aaba00
StevenLeung <loangyaoyun@gmail.com>
2026-02-16 19:48:50 +0800

    苍穹外卖第三天，通过aop实现字段注入。

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,7 +1,9 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -23,11 +25,12 @@ public interface EmployeeMapper {
      * @param employee
      */
 
+    @AutoFill(value= OperationType.INSERT)
     void insert(Employee employee);
 
     List<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
-
+    @AutoFill(value= OperationType.UPDATE)
     void update(Employee employee);
 
     Employee getById(Long id);
